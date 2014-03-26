40630e45b851c556f94809f408c0888ca66a4b10
Kaiton Williams <kaiton@cs.cornell.edu>
2014-03-26 11:26:32 -0500

    Mobile + other clean up
    - support for mobile presses
    - improved events
    - resetting cheers in UI is broken at 0.

--- app.js
+++ app.js
@@ -5,15 +5,29 @@ var port = 2014
 var cheers = []
 
 app.io.route('cheer!', function(req) {
+  // need to make sure clients are unique in max counts & room joining 
   schoolid = req.data.id
   console.log("joining room <" +schoolid+ ">")
   req.io.join(schoolid)
   console.log("registering a cheer for school <" +schoolid+ ">")
-  cheers[schoolid] = (++cheers[schoolid] || 0)
-  console.log("broadcasting cheer count [" + cheers[schoolid] + " for school <" +schoolid+ ">")
+  cheers[schoolid] = (++cheers[schoolid] || 0) // max cheers in this process lifetime
+  console.log("broadcasting cheer count = " +  app.io.sockets.clients(schoolid).length)
   
   // broadcast this to _all_ clients connected to this school room
-  app.io.room(schoolid).broadcast("cheerCount", {cheers: cheers[schoolid]})
+  app.io.room(schoolid).broadcast("cheerCount", 
+    {cheers: app.io.sockets.clients(schoolid).length}
+  )
+})
+
+app.io.route('noMoreCheers', function(req) {
+  schoolid = req.data.id
+  console.log("departing room <" +schoolid+ ">")
+  req.io.leave(schoolid)
+  // broadcast this to _all_ clients connected to this school room
+  console.log("broadcasting cheer count = " +  app.io.sockets.clients(schoolid).length)
+  app.io.room(schoolid).broadcast("cheerCount", 
+    {cheers: app.io.sockets.clients(schoolid).length}
+  )
 })
 
 
