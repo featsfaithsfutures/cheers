9a1f3f50cf8db66d318b265b6ea0849fa53b3251
Kaiton Williams <kaiton@cs.cornell.edu>
2014-03-27 14:15:05 -0500

    Servicebus updates
    - Include underscore in package.json
    - Replace in-memory schoolCheers with direction checking of socket
    rooms. This simplifies potential scaling issues if we need to go
    multi-instance for scaling. Also simplifies servicebus/local switching
    - socket sessions/rooms now can be optionally stored in service bus
    depending on config. This is turned off in Azure now until we decide to
    scale across instances.
    - made matching changes to client.html to account for backend changes
    - added a /listcheers method to dump out current cheer mapping. This is
    useful for admin, and to allow a worker to push info into DB for
    storage and visualisation.

--- app.js
+++ app.js
@@ -1,62 +1,56 @@
 var _ = require('underscore');
 app = require('express.io')()
 var nconf   = require('nconf')
-var SbStore = require('socket.io-servicebus');
 app.http().io()
 var port = process.env.PORT || 2014
 nconf.file('config.json').env();
 
-// array of array: this is a list of schools who also has a list of session ids of the people cheering.
-var schoolCheers = [];
+if ('servicebus' == nconf.get('SOCKET_STORAGE')) {
+  console.log("Using servicebus as store in production")
+  var SbStore = require('socket.io-servicebus');
+  app.io.configure(function(){
+    app.io.set('store', new SbStore({
+      topic: nconf.get("SERVICE_BUS_TOPIC"),
+      connectionString: nconf.get("SERVICE_BUS_ENDPOINT"),
+      logger: app.io.get('logger')
+    }));
+  });  
+} else {
+  console.log("using default socket.io memory store")
+}
 
 app.io.route('cheer!', function(req) {
     schoolid = req.data.id;
     if(schoolid < 0) return;
-    // check if the current socket is cheering, if cheering, add to the schoolCheer array
-    if(!_.contains(schoolCheers[schoolid], req.socket.id))
-    {
-        // TODO: Add a timeout here so we can remove this user if not cheering for the past
-        // 1000 ms and was also not manually removed by 'noMoreCheers' (if connection was dropped, we have to remove)
-        console.log("registering a cheer for school <" +schoolid+ ">");
-        schoolCheers[schoolid].push(req.socket.id);
-        // broadcast this to _all_ clients connected to this school room
-        updateRoom(schoolid);
-    }
-
+    console.log("registering a cheer for school <" +schoolid+ ">");
+    req.io.join(schoolid)
+    console.log("joined room <" +schoolid+ ">")    
+    updateRoom(schoolid);
 })
 
 app.io.route('noMoreCheers', function(req) {
     schoolid = req.data.id;
     if(schoolid < 0) return;
-
-    // check if the current socket is cheering, if cheering, remove from the schoolCheer array
-    if(_.contains(schoolCheers[schoolid], req.socket.id))
-    {
-        console.log("de-registering a cheer for school <" +schoolid+ ">");
-        // remove from array.
-        schoolCheers[schoolid] = _.without(schoolCheers[schoolid], _.findWhere(schoolCheers[schoolid], req.socket.id));
-
-        // broadcast this to _all_ clients connected to this school room
-        updateRoom(schoolid);
-    }
+    console.log("de-registering a cheer for school <" +schoolid+ ">");
+    req.io.leave(schoolid)
+    console.log("departed room <" +schoolid+ ">")
+    updateRoom(schoolid);
 })
 
 // Route for joining a school room by Id
 app.io.route('joinSchool', function(req) {
     schoolid = req.data.id
-    // create array object for new school
-    if(schoolCheers[schoolid] == null){
-        schoolCheers[schoolid] = new Array();
-    }
     console.log("joining room <" +schoolid+ ">")
     req.io.join(schoolid)
     updateRoom(schoolid);
 })
+
 // Route for leaving a school room by Id
 app.io.route('leaveSchool', function(req){
     schoolid = req.data.id
     console.log("departing room <" +schoolid+ ">")
     req.io.leave(schoolid)
+    updateRoom(schoolid);    
 })
 
 
@@ -70,6 +64,9 @@ app.get('/schools', function(req, res) {
     res.sendfile(__dirname + '/schools.json')
 })
 
+app.get('/listcheers', function(req, res){
+  res.json(getAllCurrentCheerers())
+})
 /*
 
 // stubbed out for later use when/if converting console logging to in browser
@@ -91,20 +88,25 @@ app.get('/admin', function(req, res) {
  look at setInterval(callback, delay, [arg], [...]) for running archival queue push
 */
 
-
-app.io.configure(function(){
-  app.io.set('store', new SbStore({
-    topic: nconf.get("SERVICE_BUS_TOPIC"),
-    connectionString: nconf.get("SERVICE_BUS_ENDPOINT"),
-    logger: app.io.get('logger')
-  }));
-});
-
 app.listen(port)
 
 function updateRoom(id){
-    console.log("broadcasting cheer count = " +  schoolCheers[schoolid].length);
+    console.log("broadcasting cheer count = " +   getCurrentCheerersForSchool(schoolid));
     app.io.room(id).broadcast("cheerCount",
-        {cheers: schoolCheers[schoolid].length}
+        {cheers: getCurrentCheerersForSchool(schoolid), schoolId: id}
     )
 }
+
+function getCurrentCheerersForSchool(schoolid){
+  return app.io.sockets.clients(schoolid).length
+}
+
+function getAllCurrentCheerers(){
+  result = _.inject(app.io.sockets.manager.rooms, function(all, socketids, room){
+    all[ ( room == "" ) ? "total" : room.slice(-1) ] = socketids.length     
+    return all
+  },{})
+  return result
+}
+  
+
