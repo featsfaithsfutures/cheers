1b09e212b4f91ad03822d637ae4b6e35b719224d
Shawn Mclean <shawnrmclean@gmail.com>
2014-03-27 00:20:28 -0500

    refactored the workflow:  client continuously tells the server the user is cheering once the button is being pressed.  the server keeps track of who is cheering on a particular school.  The client joins a school room when selected. Gets count when updated.

--- app.js
+++ app.js
@@ -1,35 +1,62 @@
+var _ = require('underscore');
 app = require('express.io')()
 app.http().io()
 var port = process.env.PORT || 2014
-// hacky "global" to keep track of cheers. Drops back to zero on process exit
-var cheers = []
 
+// array of array: this is a list of schools who also has a list of session ids of the people cheering.
+var schoolCheers = [];
 
 app.io.route('cheer!', function(req) {
-  // need to make sure clients are unique in max counts & room joining 
-  schoolid = req.data.id
-  console.log("joining room <" +schoolid+ ">")
-  req.io.join(schoolid)
-  console.log("registering a cheer for school <" +schoolid+ ">")
-  cheers[schoolid] = (++cheers[schoolid] || 0) // max cheers in this process lifetime
-  console.log("broadcasting cheer count = " +  app.io.sockets.clients(schoolid).length)
-  
-  // broadcast this to _all_ clients connected to this school room
-  app.io.room(schoolid).broadcast("cheerCount", 
-    {cheers: app.io.sockets.clients(schoolid).length}
-  )
+    // need to make sure clients are unique in max counts & room joining
+    schoolid = req.data.id;
+
+    // create array object for new school
+    if(schoolCheers[schoolid] == null){
+        schoolCheers[schoolid] = new Array();
+    }
+    // check if the current socket is cheering, if cheering, add to the schoolCheer array
+    if(!_.contains(schoolCheers[schoolid], req.socket.id))
+    {
+        console.log("registering a cheer for school <" +schoolid+ ">");
+        schoolCheers[schoolid].push(req.socket.id);
+        // broadcast this to _all_ clients connected to this school room
+        app.io.room(schoolid).broadcast("cheerCount",
+            {cheers: schoolCheers[schoolid].length}
+        )
+        console.log("broadcasting cheer count = " +  schoolCheers[schoolid].length);
+    }
+
 })
 
 app.io.route('noMoreCheers', function(req) {
-  schoolid = req.data.id
-  console.log("departing room <" +schoolid+ ">")
-  req.io.leave(schoolid)
-  // broadcast this to _all_ clients connected to this school room
-  console.log("broadcasting cheer count = " +  app.io.sockets.clients(schoolid).length)
-  app.io.room(schoolid).broadcast("cheerCount", 
-    {cheers: app.io.sockets.clients(schoolid).length}
-  )
-  req.io.respond({cheers: app.io.sockets.clients(schoolid).length})
+    schoolid = req.data.id;
+
+    // check if the current socket is cheering, if cheering, remove from the schoolCheer array
+    if(_.contains(schoolCheers[schoolid], req.socket.id))
+    {
+        console.log("de-registering a cheer for school <" +schoolid+ ">");
+        // remove from array.
+        schoolCheers[schoolid] = _.without(schoolCheers[schoolid], _.findWhere(schoolCheers[schoolid], req.socket.id));
+
+        // broadcast this to _all_ clients connected to this school room
+        app.io.room(schoolid).broadcast("cheerCount",
+            {cheers: schoolCheers[schoolid].length}
+        )
+        console.log("broadcasting cheer count = " +  schoolCheers[schoolid].length);
+    }
+})
+
+// Route for joining a school room by Id
+app.io.route('joinSchool', function(req) {
+    schoolid = req.data.id
+    console.log("joining room <" +schoolid+ ">")
+    req.io.join(schoolid)
+})
+// Route for leaving a school room by Id
+app.io.route('leaveSchool', function(req){
+    schoolid = req.data.id
+    console.log("departing room <" +schoolid+ ">")
+    req.io.leave(schoolid)
 })
 
 
